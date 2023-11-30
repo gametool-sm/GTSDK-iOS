6be9805a34fe5d3fae5c652844dc214ef469d29c
丰静 <fj@shangminet.net>
2023-11-30 18:53:34 +0800

    更新SMEncryption.framework

--- SMEncryption.framework/Headers/SMEncryption.h
+++ SMEncryption.framework/Headers/SMEncryption.h
@@ -1,10 +1,11 @@
 //
-//  SMEncryption.pch
+//  SMEncryption.h
 //  SMEncryption
 //
-//  Created by smwl on 2020/5/6.
-//  Copyright © 2020 smwl. All rights reserved.
+//  Created by smwl on 2023/11/30.
+//  Copyright © 2023 smwl. All rights reserved.
 //
+
 #import <Foundation/Foundation.h>
 //! Project version number for SMEncryption.
 FOUNDATION_EXPORT double SMEncryptionVersionNumber;
@@ -15,4 +16,3 @@ FOUNDATION_EXPORT const unsigned char SMEncryptionersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SMEncryption/PublicHeader.h>
 #import <SMEncryption/SMEncryptUtil.h>
 #import <SMEncryption/SMCommonUtils.h>
-
