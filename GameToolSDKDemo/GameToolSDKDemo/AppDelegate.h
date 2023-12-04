a8c1fcc6f85cea11e13a080841c95d84a1e627f4
丰静 <fj@shangminet.net>
2023-12-04 16:59:11 +0800

    新增示例demo

---
+++ GameToolSDKDemo/GameToolSDKDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GameToolSDKDemo
+//
+//  Created by smwl on 2023/12/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GameToolSDKDemo/GameToolSDKDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GameToolSDKDemo
+//
+//  Created by smwl on 2023/12/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
